12ebaba46bfe59c0789a4d3d634e00a5b2a6ba78
Max Cramer <maximiliancramer@yahoo.com>
2019-10-21 13:46:33 +0100

    adding 'fizzbuzz' for when devisable by both 3 and 5

--- script.js
+++ script.js
@@ -25,5 +25,7 @@ for(let n = 1; n <= 100; n++) {
   let output = '';
   if (n % 3 == 0) output += 'Fizz';
   if(n % 5 == 0) output += 'Buzz';
+  if(n % 5 && 3 === 0) output += 'FizzBuzz';
+
   console.log(output || n);
 }
