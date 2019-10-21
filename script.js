2812e37b7598d90a57522bf75669b862976dfe03
Max Cramer <maximiliancramer@yahoo.com>
2019-10-21 13:38:47 +0100

    adding working solution using output

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-var number = new Array(100);
-for (var i = 0; i < 100; i++) {
-  number[i] = i + 1;
-}
+// var number = new Array(100);
+// for (var i = 0; i < 100; i++) {
+//   number[i] = i + 1;
+// }
+//
+//
+//
+//
+// for (var i = 0; i < number.length; i++) {
+//   if(number[i] % 3) {
+//     console.log('fizz');
+//   }
+//   if(number[i] % 5) {
+//     console.log('buzz');
+//   }
+//   if(number[i] % 3 && 5) {
+//     console.log('fizzbuzz');
+//   } else {
+//     console.log(number[i]);
+//   }
+// }
 
-for (var i = 0; i < number.length; i++) {
-  console.log(number[i]);
+
+for(let n = 1; n <= 100; n++) {
+  let output = '';
+  if (n % 3 == 0) output += 'Fizz';
+  if(n % 5 == 0) output += 'Buzz';
+  console.log(output || n);
 }
